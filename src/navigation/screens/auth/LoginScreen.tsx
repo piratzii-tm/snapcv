14ab680c975dd1e849d809c4af37a5707b759001
Iulian Pop <iulian.pop@cobaltsign.com>
2024-11-05 18:39:45 +0200

    Implement the AI logic

--- src/navigation/screens/auth/LoginScreen.tsx
+++ src/navigation/screens/auth/LoginScreen.tsx
@@ -1,5 +1,20 @@
+import { getPersonalityRequest } from "../../../ai";
+
 export const LoginScreen = () => (
   <div className={"flex content-center items-center justify-center"}>
     <p>Here we shall have a login</p>
+    <button
+      className={
+        "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      }
+      //TODO: This is a test button, remove it
+      onClick={() =>
+        getPersonalityRequest(
+          "Ma cheam Mihi si sunt programator si im iplace sa scriu cod in React.",
+        )
+      }
+    >
+      Button
+    </button>
   </div>
 );
